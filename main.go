8c54f2748db5d2c71dd2874007998ff72037a5c9
Redson <redson@riseup.net>
2022-07-11 20:07:43 -0300

    feat: `.env` support

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"math/rand"
-	"time"
-	"fmt"
-
+  "github.com/joho/godotenv"
+  "encoding/json"
+  "net/http"
+  "math/rand"
+  "time"
+  "fmt"
+  "os"
 )
 
 func main(){
   var b []byte
-	
+  err := godotenv.Load()
+  port := os.Getenv("PORT")
+
+  if err != nil || port == "" {
+    port = "8080"
+  }
+
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     rand.Seed(time.Now().UnixNano())
     b, _ = json.Marshal(Quotes[rand.Intn( len(Quotes) )])
+
     fmt.Fprintf(w, string(b))
   })
 
-	http.ListenAndServe(":8080", nil)
+  fmt.Println("Server running on port", port)
+	http.ListenAndServe(":" + port, nil)
 }
 
