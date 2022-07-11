9884b98ed60ce535412aec42c0d7396e33be0b1e
Redson <redson@riseup.net>
2022-07-11 20:23:38 -0300

    Revert "feat: `.env` support"
    This reverts commit 8c54f2748db5d2c71dd2874007998ff72037a5c9.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module hl-api
 
 go 1.18
-
-require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,32 +1,23 @@
 package main
 
 import (
-  "github.com/joho/godotenv"
-  "encoding/json"
-  "net/http"
-  "math/rand"
-  "time"
-  "fmt"
-  "os"
+	"encoding/json"
+	"net/http"
+	"math/rand"
+	"time"
+	"fmt"
+
 )
 
 func main(){
   var b []byte
-  err := godotenv.Load()
-  port := os.Getenv("PORT")
-
-  if err != nil || port == "" {
-    port = "8080"
-  }
-
+	
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     rand.Seed(time.Now().UnixNano())
     b, _ = json.Marshal(Quotes[rand.Intn( len(Quotes) )])
-
     fmt.Fprintf(w, string(b))
   })
 
-  fmt.Println("Server running on port", port)
-	http.ListenAndServe(":" + port, nil)
+	http.ListenAndServe(":8080", nil)
 }
 
