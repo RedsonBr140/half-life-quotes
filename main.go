93d60fa1c2c4fc72774658f90fd63dbb195414f1
Redson <redson@riseup.net>
2022-07-12 14:11:41 -0300

    a

--- main.go
+++ main.go
@@ -9,23 +9,23 @@ import (
 "time"
 )
 
+var b []byte
+
+func Handler(w http.ResponseWriter, r *http.Request){
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+  rand.Seed(time.Now().UnixNano())
+
+  b, _ = json.Marshal(Quotes[rand.Intn( len(Quotes) )])
+  fmt.Fprintf(w, string(b))
+}
+
 func main(){
-  var(
-    port string = os.Getenv("PORT")
-    b    []byte
-  )
+  port := os.Getenv("PORT")
 
   if port == "" {
     port = "8080"
   }
-
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    rand.Seed(time.Now().UnixNano())
-    b, _ = json.Marshal(Quotes[rand.Intn( len(Quotes) )])
-    fmt.Fprintf(w, string(b))
-  })
-
+  http.HandleFunc("/", Handler)
   fmt.Println("Server running on port", port)
   http.ListenAndServe(":" + port, nil)
 }
