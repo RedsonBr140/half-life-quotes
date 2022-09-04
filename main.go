11ea703276919feb7059ccac567da3fb1accfdc5
Redson <redson@riseup.net>
2022-09-04 17:44:22 -0300

    chore: gofmt

--- main.go
+++ main.go
@@ -1,62 +1,61 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "time"
-  "log"
-  "math/rand"
-  "net/http"
-  "strconv"
-  "os"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func GetRandomQuote(quotesNumber int) []quote {
-  var limit int
-  var (
-    output      []quote
-    randomquote quote
-  )
+	var limit int
+	var (
+		output      []quote
+		randomquote quote
+	)
 
-  if quotesNumber > len(Quotes) {
-    limit = len(Quotes)
-  } else {
-    limit = quotesNumber
-  }
+	if quotesNumber > len(Quotes) {
+		limit = len(Quotes)
+	} else {
+		limit = quotesNumber
+	}
 
-  rand.Seed(time.Now().UnixNano())
-  for i:=0;i<limit;i++ {
-    randomquote = Quotes[rand.Intn(len(Quotes))]
-    output = append(output, randomquote)
-  }
-  return output
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < limit; i++ {
+		randomquote = Quotes[rand.Intn(len(Quotes))]
+		output = append(output, randomquote)
+	}
+	return output
 }
 
-func jsonQuotesHandler(w http.ResponseWriter,r *http.Request) {
-  w.Header().Set("Access-Control-Allow-Origin", "*")
-  vars := mux.Vars(r)
-  // Converting string to int so we can compare
-  number, _ := strconv.Atoi(vars["quotes"])
-  if number == 0 {
-    number = 1
-  }
+func jsonQuotesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	vars := mux.Vars(r)
+	// Converting string to int so we can compare
+	number, _ := strconv.Atoi(vars["quotes"])
+	if number == 0 {
+		number = 1
+	}
 
-  json, _ := json.Marshal(GetRandomQuote(number))
-  fmt.Fprintf(w, string(json))
+	json, _ := json.Marshal(GetRandomQuote(number))
+	fmt.Fprintf(w, string(json))
 }
 
-func main(){
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-  }
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-  r := mux.NewRouter()
-  r.HandleFunc("/", jsonQuotesHandler)
-  r.HandleFunc("/{quotes}", jsonQuotesHandler).Methods("GET")
-  fmt.Println("Server running on port", port)
-  log.Fatal(http.ListenAndServe(":" + port, r))
+	r := mux.NewRouter()
+	r.HandleFunc("/", jsonQuotesHandler)
+	r.HandleFunc("/{quotes}", jsonQuotesHandler).Methods("GET")
+	fmt.Println("Server running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
-
--- quotes.go
+++ quotes.go
@@ -1,7 +1,6 @@
 package main
 
-
-type quote struct{
+type quote struct {
 	Quote  string `json:"quote"`
 	Author string `json:"author"`
 }
@@ -20,20 +19,19 @@ var Quotes = []quote{
 	{"Morphine administered.", "HEV Suit"},
 
 	{"", "Gordon Freeman"}, // Yes guys, I did that.
-  {"Have you heard? They are killing scientists... our own race, turning against us.", "Unnamed scientist"},
-  {"Gordon doesn't need to hear all this, he's a highly trained professional!", "Unnamed scientist"}, // Of course he is.
-  {"For god's sake open the silo doors!", "Unnamed scientist"},
-  {"Oh, my GOD, WE'RE DOOMED!", "Unnamed scientist"},
+	{"Have you heard? They are killing scientists... our own race, turning against us.", "Unnamed scientist"},
+	{"Gordon doesn't need to hear all this, he's a highly trained professional!", "Unnamed scientist"}, // Of course he is.
+	{"For god's sake open the silo doors!", "Unnamed scientist"},
+	{"Oh, my GOD, WE'RE DOOMED!", "Unnamed scientist"},
 
-  {"Morning Mr.Freeman, looks like you're running a late", "Barney Calhoun"},
+	{"Morning Mr.Freeman, looks like you're running a late", "Barney Calhoun"},
 
-  {"Forget about Freeman, we are cutting our losses and pulling out! Anyone left down there now is on his own.", "HECU Captain"},
-  {"I didn't sign on for this shit. Monsters, sure, but civilians?", "HECU soldier"},
-  {"All I know for sure is he's been killing my buddies", "HECU soldier"},
-  {"Body? What body?", "HECU soldier"},
+	{"Forget about Freeman, we are cutting our losses and pulling out! Anyone left down there now is on his own.", "HECU Captain"},
+	{"I didn't sign on for this shit. Monsters, sure, but civilians?", "HECU soldier"},
+	{"All I know for sure is he's been killing my buddies", "HECU soldier"},
+	{"Body? What body?", "HECU soldier"},
 
-  {"I have recommended your services to my … employers, and they have authorized me to offer you a job.", "G-Man"},
-  {"Quite a nasty piece of work you managed over there, I am impressed.","G-Man"},
-  {"Gordon Freeman, in the flesh - or rather, in the hazard suit.", "G-Man"},
+	{"I have recommended your services to my … employers, and they have authorized me to offer you a job.", "G-Man"},
+	{"Quite a nasty piece of work you managed over there, I am impressed.", "G-Man"},
+	{"Gordon Freeman, in the flesh - or rather, in the hazard suit.", "G-Man"},
 }
-
